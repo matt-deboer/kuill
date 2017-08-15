2873ae6dae6d0af91ac5493cd666d883b2425a2e
Matt DeBoer <matt.deboer@gmail.com>
2017-08-15 11:24:36 -0700

    disable unused area charts

--- pkg/ui/src/components/dashboard/InfoBox.js
+++ pkg/ui/src/components/dashboard/InfoBox.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ResponsiveContainer, AreaChart, Area } from 'recharts'
+// import { ResponsiveContainer, AreaChart, Area } from 'recharts'
 import Paper from 'material-ui/Paper'
 import { white, grey800 } from 'material-ui/styles/colors'
 import typography from 'material-ui/styles/typography'
@@ -90,25 +90,15 @@ export default class InfoBox extends React.PureComponent {
         fill: 'rgba(255,255,255,0.7)',
       }
     }
-    
-    const data = [
-          {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-          {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-          {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-          {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-          {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-          {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-          {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-    ]
 
     return (
       <Paper style={styles.wrapper} className={'infobox'}>
 
-        <ResponsiveContainer style={{}}>
+        {/* <ResponsiveContainer style={{}}>
           <AreaChart data={data} margin={{top: 5, right: 0, left: 0, bottom: 5}}>
             <Area type='monotone' dataKey='uv' stroke='rgb(41, 98, 255)' fill='rgba(41, 98, 255, 0.3)' />
           </AreaChart>
-        </ResponsiveContainer>        
+        </ResponsiveContainer>         */}
         
         <span style={styles.iconSpan}>
           <span style={styles.text}>{title}</span>
