388f9f1abd1b23330a889fa9528a71a7c2ffd3cd
Matt DeBoer <matt.deboer@gmail.com>
2017-08-18 22:42:49 -0700

    make chart more resilient to zero values

--- pkg/ui/src/components/dashboard/UtilizationPieChart.js
+++ pkg/ui/src/components/dashboard/UtilizationPieChart.js
@@ -8,20 +8,36 @@ export default class UtilizationPieChart extends React.PureComponent {
   render() {
     let { total, used, label, percent } = this.props
 
-    if (!total) {
-      return null
-    }
-
+    // if (!total) {
+    //   return null
+    // }
+    let data = []
+    let value = 0
+    
     let pct = 0
-    if (percent) {
-      pct = (100 * used / total)
-      if (pct < 0.1) {
-        pct = pct.toFixed(2).substr(1)
-      } else if (pct < 1) {
-        pct = pct.toFixed(1)
+    if (total > 0) {
+      if (percent) {
+        pct = (100 * used / total)
+        if (pct < 0.1) {
+          pct = pct.toFixed(2).substr(1)
+        } else if (pct < 1) {
+          pct = pct.toFixed(1)
+        } else {
+          pct = Math.round(pct)
+        }
+        value = pct
       } else {
-        pct = Math.round(pct)
+        value = used
       }
+      data = [
+        {name: 'used', value: used},
+        {name: 'free', value: (total - used)},
+      ]
+    } else {
+      data = [
+        {name: 'used', value: 0},
+        {name: 'free', value: 100},
+      ]
     }
 
     return (
@@ -32,10 +48,7 @@ export default class UtilizationPieChart extends React.PureComponent {
               <Pie
                 innerRadius={35}
                 outerRadius={42}
-                data={[
-                  {name: 'used', value: used},
-                  {name: 'free', value: (total - used)},
-                ]}
+                data={data}
                 fill="#8884d8"
                 stroke="none">
                 <Cell key={'used'} fill={blueA400}/>
@@ -44,10 +57,7 @@ export default class UtilizationPieChart extends React.PureComponent {
             </PieChart>
           </ResponsiveContainer>
           <div className="utilization">
-            {percent ? 
-            <div className="percentage">{pct}</div> :
-            <div className="used">{used}</div>
-            }
+            <div className={percent ? 'percentage' : 'used'}>{value}</div>
             <div className="label">{label}</div>
           </div>
       </div>
