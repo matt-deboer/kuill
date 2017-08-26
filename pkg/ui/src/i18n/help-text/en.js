1a17a15b64f5d7c46f8b5e2997305d050eae8185
Matt DeBoer <matt.deboer@gmail.com>
2017-08-26 12:04:13 -0700

    initial i18n support, help text labels

---
+++ pkg/ui/src/i18n/help-text/en.js
@@ -0,0 +1,4 @@
+export default {
+  NamespaceBarChart: "This is the current count of each reasource, filtered by the namespaces you've selected",
+  ResourceCounts: "This is the current count of each reasource, filtered by the namespaces you've selected",
+}
