116c5012b8f4f677455f5dde5a9247242564c21d
Matt DeBoer <matt.deboer@gmail.com>
2017-08-27 20:51:18 -0700

    helptext updates for nodes page

--- pkg/ui/src/i18n/help-text/en.js
+++ pkg/ui/src/i18n/help-text/en.js
@@ -2,11 +2,15 @@ export default {
   'NamespaceBarChart.barchart': "Shows relative usage (by namespace) of the resource selected (to the right); " +
     "click to select a namespace (shift+click to select multiples), and problems, events, counts, etc. on " +
     "this page will be limited to the selected namespace(s)",
-  'NamespaceBarChart.donuts': "Shows the cumulative usage (over all selected namespaces) of a subset of resources; " +
+  'NamespaceBarChart.donuts': "Shows the cumulative usage (over all selected namespaces) of a specific resource; " +
     "click to select a resource, and the barchart to the left will be updated to show relative usage of that resource " +
     "by the individual namespace(s) selected",
-  'ResourceCounts': "This is the current count of each reasource, filtered by the namespaces you've selected",
-  'ResourceProblems': "Shows current problem resources (separated by workload and cluster), along with the " +
+  'ResourceCounts': "The current count of each reasource, filtered by the namespaces you've selected",
+  'ResourceProblems': "Current problem resources (separated by workload and cluster), along with the " +
     "most recent events received for those resources",
-  'RecentUpdates': "This shows the 25 most recent events received (filtered by the namespaces you've selected)",
+  'RecentUpdates': "The 25 most recent events received (filtered by the namespaces you've selected)",
+  'NodeHeatmap.heatmap': "Heatmap depicts resource usage for the resource selected (to the right), where 1 hexagon = 1 node",
+  'NodeHeatmap.donuts': "Shows the cumulative usage (over all selected nodes) of a specific resource; " +
+    "click to select a resource, and the heatmap to the left will be updated to show relative usage of that " +
+    "resource by the individual nodes selected",
 }
