c3be6959b064e7056b52622eec7c6442cb36620e
Matt DeBoer <matt.deboer@gmail.com>
2017-09-13 08:24:38 -0700

    extra indent for secondary sequence elements

--- pkg/ui/src/components/EditorYamlMode.js
+++ pkg/ui/src/components/EditorYamlMode.js
@@ -239,6 +239,8 @@ ace.define("ace/mode/kube_yaml_highlight_rules",["require","exports","module","a
               var match = line.match(/^.*[{([]\s*$/);
               if (match) {
                   indent += tab;
+              } else if (line.match(/^\s*-.*$/)) {
+                  indent += tab;
               }
           }
   
