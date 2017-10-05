3ac937c830b344a493b703ed8164a18409f143df
Matt DeBoer <matt.deboer@gmail.com>
2017-10-05 14:50:06 -0700

    use a consistent base64 decode

--- hack/get-certs.sh
+++ hack/get-certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p ./certs
-kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."auth-proxy.pem"' | base64 -D > ./certs/server-cert.pem
-kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."auth-proxy-key.pem"' | base64 -D > ./certs/server-key.pem
-kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."ca.pem"' | base64 -D > ./certs/ca.pem
+kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."auth-proxy.pem"' | openssl base64 -d > ./certs/server-cert.pem
+kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."auth-proxy-key.pem"' | openssl base64 -d > ./certs/server-key.pem
+kubectl get secret -n kube-system auth-proxy-certs -o json | jq -r '.data."ca.pem"' | openssl base64 -d > ./certs/ca.pem
