102fd2da941067a5a5bdb3bffad7476d6a678206
Matt DeBoer <matt.deboer@gmail.com>
2017-10-04 23:55:30 -0700

    add to the libvirtd group

--- hack/travis/setup.sh
+++ hack/travis/setup.sh
@@ -11,3 +11,7 @@ chmod +x minikube && sudo mv minikube /usr/local/bin/
 # install docker-machine-driver-kvm
 curl -Lo docker-machine-driver-kvm https://github.com/dhiltgen/docker-machine-kvm/releases/download/v0.10.0/docker-machine-driver-kvm-ubuntu14.04
 chmod +x docker-machine-driver-kvm && sudo mv docker-machine-driver-kvm /usr/local/bin/
+
+# add current user to the libvirtd group and update the session
+sudo usermod -a -G libvirtd $(whoami)
+newgrp libvirtd
