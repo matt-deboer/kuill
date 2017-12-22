ee1728ec6cc9165f44d20d2558a3a3b5827e3eb6
Matt DeBoer <matt.deboer@gmail.com>
2017-12-22 08:10:21 -0800

    no more need for auth-proxy flags

--- hack/test-drive-minikube.sh
+++ hack/test-drive-minikube.sh
@@ -10,11 +10,6 @@ if [ -z "$(echo $status | grep 'minikube: Running')" ]; then
   ${MINIKUBE_SUDO} minikube start ${MINIKUBE_OPTIONS} \
    --kubernetes-version v1.8.0 \
     --extra-config apiserver.Authorization.Mode=RBAC \
-    --extra-config apiserver.Authentication.RequestHeader.AllowedNames=auth-proxy \
-    --extra-config apiserver.Authentication.RequestHeader.ClientCAFile=/var/lib/localkube/certs/ca.crt \
-    --extra-config apiserver.Authentication.RequestHeader.UsernameHeaders=X-Remote-User \
-    --extra-config apiserver.Authentication.RequestHeader.GroupHeaders=X-Remote-Group \
-    --extra-config apiserver.Authentication.RequestHeader.ExtraHeaderPrefixes=X-Remote-Extra- \
     --extra-config apiserver.Audit.LogOptions.Path=/var/log/apiserver-audit.log \
     --extra-config apiserver.Audit.LogOptions.MaxAge=1 \
     --extra-config apiserver.Audit.LogOptions.MaxSize=50 \
